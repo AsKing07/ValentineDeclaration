e6010a0830814daeed393b4a995ae2f2be50143f
Charbel SONON <charbelsnn@gmail.com>
2025-02-13 16:16:33 +0100

    update

--- script/main.js
+++ script/main.js
@@ -280,7 +280,7 @@ const animationTimeline = () => {
 
 // Import the data to customize and insert them into page
 const fetchData = () => {
-  fetch("customize.json")
+  fetch("./customize.json")
     .then((data) => data.json())
     .then((data) => {
       Object.keys(data).map((customData) => {
